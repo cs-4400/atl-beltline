c51cc06564f31758482aecac426c2e5e4b8274ad
mpham33 <mpham33@gatech.edu>
2019-04-04 21:26:47 -0400

    example create statements up

--- db/init_db.py
+++ db/init_db.py
@@ -1,14 +1,47 @@
-import MySQLdb as mysql
+import pymysql as mysql
 
-# connect to server
-conn = mysql.connect(host='localhost', user='root', password='root')
+# connect to server to create DB
+db = mysql.connect(host='localhost', user='root', password='root')
+cur = db.cursor()
 
-cur = conn.cursor()
+# IF the database already exists, DROP
+cur.execute("DROP DATABASE IF EXISTS atl_beltline")
 
 # create database
-create_db = 'CREATE DATABASE atl_beltline'
-
+create_db = """CREATE DATABASE atl_beltline"""
 cur.execute(create_db)
+
+# connect to server to DB to create TABLES
+table = mysql.connect(host='localhost', user='root', password='root', db='atl_beltline')
+cur = table.cursor()
 
 # CREATE statements here
 
+# USER TABLE
+cur.execute("DROP TABLE IF EXISTS user")
+user_table = """
+CREATE TABLE user (
+    username varchar(255) NOT NULL,
+    lname varchar(255) NOT NULL,
+    fname varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    status ENUM('Approved','Pending', 'Declined'),
+    usertype ENUM('Employee','Visitor'),
+    PRIMARY KEY (username))
+"""
+
+cur.execute(user_table)
+
+# USER_EMAIL TABLE
+cur.execute("DROP TABLE IF EXISTS user_email")
+email_table = """
+CREATE TABLE user_email (
+    username varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    PRIMARY KEY (email),
+    UNIQUE (email),
+    FOREIGN KEY (username) REFERENCES user (username)
+    ON DELETE CASCADE ON UPDATE CASCADE)
+"""
+
+cur.execute(email_table)
